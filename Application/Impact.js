67b14628b74c11a94736cd1dacda4dc96495a917
Bast <bastien.last87@gmail.com>
2020-08-27 14:46:07 +0200

    first structure of the app

---
+++ Application/Impact.js
@@ -0,0 +1,10 @@
+class Impact {
+
+    constructor(idSituation, idArgument) {
+        this.motivationPenality = 0;
+        this.frustration = 0;
+        this.equilibreAlim = 0;
+        this.sante = 0;
+    }
+
+}
---
+++ Application/View.js
@@ -0,0 +1,6 @@
+class View {
+    constructor() { }
+
+
+
+}
