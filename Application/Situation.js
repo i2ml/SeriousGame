276fa48ae04a6987488b362debc32700c3d5e888
Bast <bastien.last87@gmail.com>
2020-08-27 14:44:58 +0200

    Merged situation data in a loadable file

---
+++ Application/Situation.js
@@ -0,0 +1,11 @@
+class Situation {
+
+    constructor(id) {
+        this.enonce = "";
+        this.arguments = new Array();
+        array.forEach(element => {
+            this.arguments.push(new Argument(id,));
+        });
+    }
+
+}
