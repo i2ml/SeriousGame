cfd8a4a2f79ce62547814d327a206359832ac18c
Bast <bastien.last87@gmail.com>
2020-08-28 14:37:00 +0200

    constructeurs

--- Application/Argument.js
+++ Application/Argument.js
@@ -1,10 +1,10 @@
 class Argument {
 
     constructor(idSituation, id) {
-        this.enonce = "";
-        this.motivationNeeded = 0;
-        this.success = new Impact(idSituation, id);
-        this.failure = new Impact(idSituation, id);
+        this.enonce = situations[idSituation].arguments[id].enonce;
+        this.motivationNeeded = situations[idSituation].arguments[id].motivationNeeded;
+        this.success = new Impact(idSituation, id, true);
+        this.failure = new Impact(idSituation, id, false);
     }
 
 }
--- Application/Impact.js
+++ Application/Impact.js
@@ -1,10 +1,16 @@
 class Impact {
 
-    constructor(idSituation, idArgument) {
-        this.motivationPenality = 0;
-        this.frustration = 0;
-        this.equilibreAlim = 0;
-        this.sante = 0;
+    constructor(idSituation, idArgument, isSuccess) {
+        if (isSuccess) {
+            this.motivationPenality = situations[idSituation].arguments[idArgument].success.motivationPenality;
+            this.frustration = situations[idSituation].arguments[idArgument].success.frustration;
+            this.equilibreAlim = situations[idSituation].arguments[idArgument].success.equilibreAlim;
+            this.sante = situations[idSituation].arguments[idArgument].success.sante;
+        } else {
+            this.motivationPenality = situations[idSituation].arguments[idArgument].failure.motivationPenality;
+            this.frustration = situations[idSituation].arguments[idArgument].failure.frustration;
+            this.equilibreAlim = situations[idSituation].arguments[idArgument].failure.equilibreAlim;
+            this.sante = situations[idSituation].arguments[idArgument].failure.sante;
+        }
     }
-
 }
