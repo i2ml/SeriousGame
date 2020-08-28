f038316e165ada4b6f19d8692da724d1cd8ff28c
Bast <bastien.last87@gmail.com>
2020-08-28 11:37:52 +0200

    small bugfix

--- Application/Argument.js
+++ Application/Argument.js
@@ -3,8 +3,8 @@ class Argument {
     constructor(idSituation, id) {
         this.enonce = "";
         this.motivationNeeded = 0;
-        this.success = new Impact(idSituation, idArgument);
-        this.failure = new Impact(idSituation, idArgument);
+        this.success = new Impact(idSituation, id);
+        this.failure = new Impact(idSituation, id);
     }
 
 }
