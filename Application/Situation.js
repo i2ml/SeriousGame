76d44c63c89ff78bcc8d35aa170c4132e4bcdd34
Bast <bastien.last87@gmail.com>
2020-08-28 14:37:00 +0200

    constructeurs

--- Application/Situation.js
+++ Application/Situation.js
@@ -1,7 +1,6 @@
 class Situation {
 
     constructor(id) {
-        console.log(situations[id].arguments)
         this.enonce = situations[id].enonce;
         this.arguments = new Array();
         for (const argumentId in situations[id].arguments) {
