e53e132ed8d52882ed91643c72e7c697a61a165e
Bast <bastien.last87@gmail.com>
2020-09-03 10:40:04 +0200

    Added argument selection

--- Application/Model.js
+++ Application/Model.js
@@ -6,7 +6,27 @@ class Model {
     }
 
     loadNextSituation() {
+        const argumentToLoad = this.choseFinalArgumentId();
+        console.log("The chosen argument is " + argumentToLoad)
         console.log("loading the situation : " + this.situation.id++)
         this.situation = new Situation(this.situation.id++);
     }
+
+    choseFinalArgumentId() {
+        const inputList = $("input")
+        let argumentsList = new Array();
+        const numberOfArguments = inputList.length / 2;
+        for (const key in inputList) {
+            if (inputList.hasOwnProperty(key)) {
+                const element = inputList[key];
+                if ($(element).is(":checked")) {
+                    let argumentId = key >= numberOfArguments ? parseInt(key) - numberOfArguments : parseInt(key);
+                    argumentId++;
+                    argumentsList.push(argumentId);
+                }
+            }
+        }
+        return argumentsList[Math.floor(Math.random() * argumentsList.length)];
+    }
+
 }
