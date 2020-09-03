0c39712f5739157b0afc37d124cff69bcc111794
Bast <bastien.last87@gmail.com>
2020-09-03 11:06:24 +0200

    disable confirm on invalid input

--- Application/Controller.js
+++ Application/Controller.js
@@ -13,6 +13,25 @@ let app = new Controller(new Model(), new View());
 app.startGame();
 
 $("#valider").click(function () {
+    if ($("#valider").hasClass("disabled")) {
+        return;
+    }
     app.model.loadNextSituation()
     app.view.displaySituation(app.model.situation);
 })
+
+$("input").click(function () {
+    const inputList = $("input")
+    let first = 0, second = 0;
+    const numberOfArguments = inputList.length / 2;
+    for (const key in inputList) {
+        if (inputList.hasOwnProperty(key)) {
+            const element = inputList[key];
+            if ($(element).is(":checked")) {
+                key >= numberOfArguments ? first++ : second++;
+            }
+        }
+    }
+    (first > 0 && second > 0) ? $("#valider").removeClass("disabled") : $("#valider").addClass("disabled");
+})
+
