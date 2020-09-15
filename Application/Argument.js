e660a5c58880553e42632ae7a156c5c74739daf4
Bast <bastien.last87@gmail.com>
2020-09-16 01:03:44 +0200

    added needed value

--- Application/Argument.js
+++ Application/Argument.js
@@ -1,6 +1,7 @@
 class Argument {
 
     constructor(idSituation, id) {
+        this.motherSituation = idSituation;
         this.enonce = situations[idSituation].arguments[id].enonce;
         this.energieNeeded = situations[idSituation].arguments[id].energieNeeded;
         this.success = new Impact(idSituation, id, true);
