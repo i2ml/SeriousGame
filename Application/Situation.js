ecdabf5c7782fa65fa32ec8901f11a349f821bc0
BastLast <bastien.last87@gmail.com>
2021-02-18 10:01:30 +0100

    petit refactoring

--- Application/Situation.js
+++ Application/Situation.js
@@ -3,10 +3,10 @@ class Situation {
     constructor(id) {
         this.id = id;
         this.enonce = situations[id].enonce;
-        this.arguments = new Array();
+        this.arguments = [];
         for (const argumentId in situations[id].arguments) {
             this.arguments.push(new Argument(id, argumentId));
-        };
+        }
     }
 
 }
